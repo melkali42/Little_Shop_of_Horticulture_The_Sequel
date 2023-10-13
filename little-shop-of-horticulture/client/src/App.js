4e5b12c1a212a2508a47f0ed4d414ebad2891bbf
cris <cristinanevelits@yahoo.com>
2023-10-12 21:14:36 -0700

    delete info

--- little-shop-of-horticulture/client/src/App.js
+++ little-shop-of-horticulture/client/src/App.js
@@ -8,25 +8,4 @@ import ProductList from './components/productList';
 import Signup from './components/signup';
 
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </div>
-    </div>
-  );
-}
-
 export default App;
