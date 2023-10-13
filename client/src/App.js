7391a3c277473f44af453e3096fcc4dd45cd8e4d
cris <cristinanevelits@yahoo.com>
2023-10-12 19:58:36 -0700

    edits for react

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,8 @@
-import './style.css';
+import Cart from './components/cart';
+import Favorites from './components/favorites';
+import Home from './components/home';
+import Login from './components/login';
+import ProductInfo from './components/productInfo';
+import ProductList from './components/productList';
+import Signup from './components/signup';
+import './index.css';
