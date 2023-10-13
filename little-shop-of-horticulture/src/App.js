3e8ef499dd29c3da27a9c3ea4527aab2c55f74ce
cris <cristinanevelits@yahoo.com>
2023-10-12 20:34:01 -0700

    changed dom from client and header to div

--- little-shop-of-horticulture/src/App.js
+++ little-shop-of-horticulture/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div>
     </div>
   );
 }
