24311b61ca66a84561b1774235654b1795b70b6c
cris <cristinanevelits@yahoo.com>
2023-10-12 20:50:30 -0700

    moving components into the react app folder (???)

--- client/src/App.js
+++
@@ -1,8 +0,0 @@
-import Cart from './components/cart';
-import Favorites from './components/favorites';
-import Home from './components/home';
-import Login from './components/login';
-import ProductInfo from './components/productInfo';
-import ProductList from './components/productList';
-import Signup from './components/signup';
-import './index.css';
