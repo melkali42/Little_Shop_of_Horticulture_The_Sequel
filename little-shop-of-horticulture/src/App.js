727ec070dc754fd2b37b97acdf9b9ed31e9ac2e4
cris <cristinanevelits@yahoo.com>
2023-10-12 21:00:30 -0700

    import

--- little-shop-of-horticulture/src/App.js
+++ little-shop-of-horticulture/src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Cart from './components/cart';
+import Favorites from './components/favorites';
+import Home from './components/home';
+import Login from './components/login';
+import ProductInfo from './components/productInfo';
+import ProductList from './components/productList';
+import Signup from './components/signup';
+
 
 function App() {
   return (
