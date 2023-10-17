bc65174003ed7e4c5853702ab59978d2a8139021
cris <cristinanevelits@yahoo.com>
2023-10-16 19:39:12 -0700

    delete old folder

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+import Cart from './components/cart';
+import Favorites from './components/favorites';
+import Home from './components/home';
+import Login from './components/login';
+import ProductInfo from './components/productInfo';
+import ProductList from './components/productList';
+import Signup from './components/signup';
+
+
+export default App;
