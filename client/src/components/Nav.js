dc994bd4760ddf51f93bb9f58631f8cdcff4a663
cris <cristinanevelits@yahoo.com>
2023-10-18 20:47:28 -0700

    removed signup in navbar since its linked in login

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -33,7 +33,6 @@ export default function Navigation() {
                             <Nav.Link as={Link} to='/'>Home</Nav.Link>
                             <Nav.Link as={Link} to='/productList'>Product List</Nav.Link>
                             <Nav.Link as={Link} to='/login'>Login</Nav.Link>
-                            <Nav.Link as={Link} to='/signup'>Signup</Nav.Link>
                             <Nav.Link as={Link} to='/about'>About</Nav.Link>
                         </Nav>
                     </Container>
