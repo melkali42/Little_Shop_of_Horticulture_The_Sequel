cd9a7349a2fc3697178ff53c4c2ed469b5dea5e9
Melissa <melissakalish2@gmail.com>
2023-10-18 20:28:12 -0500

    error fix for heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/little-shop-db', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/little-shop-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
  
