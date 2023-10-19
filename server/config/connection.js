b6097e071de7893ac0b85ad0bbec653aefe4bc74
Melissa <melissakalish2@gmail.com>
2023-10-18 21:00:44 -0500

    fixed connection errors for mongodb and heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,26 +1,12 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/little-shop-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
- 
-});
-
-module.exports = mongoose.connection;
-
-
-
-
-
 // made some revisions to initialize db connection
 
-// const { connect, connection } = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-// const connectDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/little-shop-db';
+const connectDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/little-shop-db';
 
-// connect(connectDB);
+connect(connectDB);
 
-// module.exports = connection;
+module.exports = connection;
 
 
 
