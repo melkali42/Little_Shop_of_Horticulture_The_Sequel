d013042ec9a23d58bc017b4a8b0764192b950874
Melissa <melissakalish2@gmail.com>
2023-10-18 20:10:11 -0500

    deleted space

--- server/server.js
+++ server/server.js
@@ -60,7 +60,6 @@ const startApolloServer = async () => {
 
     db.once('open', () => {
         app.listen(PORT, () => {
-
             console.log(`API server running on port ${PORT}!`);
             console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
         });
